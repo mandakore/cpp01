05aa7991b7b85560226578788583cac6dce4fb20
Tashiro Atsuya <galaxyfish11@gmail.com>
2026-03-13 17:54:14 +0900

    a

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -10,3 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+#include <iostream>
+
+int main() {
+	Harl harl;
+
+	std::cout << "--- Testing DEBUG ---" << std::endl;
+	harl.complain("DEBUG");
+	std::cout << std::endl;
+
+	std::cout << "--- Testing INFO ---" << std::endl;
+	harl.complain("INFO");
+	std::cout << std::endl;
+
+	std::cout << "--- Testing WARNING ---" << std::endl;
+	harl.complain("WARNING");
+	std::cout << std::endl;
+
+	std::cout << "--- Testing ERROR ---" << std::endl;
+	harl.complain("ERROR");
+	std::cout << std::endl;
+
+	std::cout << "--- Testing UNKNOWN ---" << std::endl;
+	harl.complain("INVALID_LEVEL");
+	std::cout << std::endl;
+
+	return 0;
+}
