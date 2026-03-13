bbbc93774af9a0bcdd70df3148467ec9c120267c
mandakore <galaxyfish11@gmail.com>
2026-03-13 19:20:48 +0900

    a

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 15:45:49 by atashiro          #+#    #+#             */
-/*   Updated: 2026/03/13 18:17:29 by atashiro         ###   ########.fr       */
+/*   Updated: 2026/03/13 19:13:42 by atashiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,26 @@ int	main() {
 
 	return 0;
 }
+
+// #include <iostream>
+// #include <string>
+
+// int main() {
+// 	std::string str1 = "ONE";
+// 	std::string str2 = "TWO";
+// 	std::string &ref = str1;
+
+// 	std::cout << "--- 代入前 ---" << std::endl;
+// 	std::cout << "str1のアドレス: " << &str1 << " 値: " << str1 << std::endl;
+// 	std::cout << "str2のアドレス: " << &str2 << " 値: " << str2 << std::endl;
+// 	std::cout << "ref のアドレス: " << &ref  << " 値: " << ref << std::endl;
+
+
+// 	ref = str2;
+
+// 	std::cout << "\n--- 代入 (ref = str2) 後 ---" << std::endl;
+// 	std::cout << "ref のアドレス: " << &ref  << " 値: " << ref << std::endl;
+// 	std::cout << "str1のアドレス: " << &str1 << " 値: " << str1 << std::endl;
+
+// 	return 0;
+// }
